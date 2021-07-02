cd725547909b1d5b74c0c84f7cd418e0e6f98903
Rajat <rajatsardesai@gmail.com>
2021-07-02 14:17:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import axios from "axios";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+
+const countriesURL = "https://restcountries.eu/rest/v2/all";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  cursor: {
+    cursor: "pointer",
+    border: "1px solid #000"
+  }
+});
+
+function App() {
+  const [countriesData, setCountriesData] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState([]);
+
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const classes = useStyles();
+
+  const getCountriesWithAxios = async () => {
+    const response = await axios.get(countriesURL);
+    setCountriesData(response.data);
+    setCountriesData(response.data);
+  };
+
+  useEffect(() => {
+    getCountriesWithAxios();
+  }, []);
+
+  const selectedData = (data) => {
+    setSelectedCountry(data);
+  }
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
+
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={12}>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table" responsive="sm">
+              <TableHead className="thead">
+                <TableRow>
+                  <TableCell>
+                    <strong>Name</strong>
+                  </TableCell>
+                  <TableCell align="right">
+                    <strong>Flag</strong>
+                  </TableCell>
+                  <TableCell align="right">
+                    <strong>Capital</strong>
+                  </TableCell>
+                  <TableCell align="right">
+                    <strong>Population</strong>
+                  </TableCell>
+                  <TableCell align="right">
+                    <strong>Region</strong>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {countriesData.map((country, index) => (
+                  ['right'].map((anchor) =>
+                    <TableRow onClick={() => selectedData(country)} onClickCapture={toggleDrawer(anchor, true)} className={classes.cursor} key={index}>
+                      <TableCell component="th" data-heading="Name" className="td" scope="row">{country.name}</TableCell>
+                      <TableCell align="right" data-heading="Flag" className="td">
+                        <img src={process.env.PUBLIC_URL + country.flag} alt="Flag" width="32px" />
+                      </TableCell>
+                      <TableCell align="right" data-heading="Capital" className="td">
+                        {country.capital}
+                      </TableCell>
+                      <TableCell align="right" data-heading="Population" className="td">{country.population}</TableCell>
+                      <TableCell align="right" data-heading="Region" className="td">{country.region}</TableCell>
+                    </TableRow>
+                  )))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
+
+      <Sidebar toggleDrawer={toggleDrawer} state={state} selectedCountry={selectedCountry} />
+
+    </>
+  );
+
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+    list: {
+        width: 450,
+    },
+    fullList: {
+        width: 'auto',
+    },
+    width: {
+        width: '100%'
+    },
+    border: {
+        borderBottom: '0.5px solid #ccc',
+        width: '100%'
+    }
+}));
+
+const Sidebar = (props) => {
+    const classes = useStyles();
+
+    const list = (anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={props.toggleDrawer(anchor, false)}
+            onKeyDown={props.toggleDrawer(anchor, false)}
+        >
+            <List>
+                <ListItem button>
+                    <Box display="flex" flexDirection="column" className={classes.width}>
+                        <h1>{props.selectedCountry.name}</h1>
+                        <hr className={classes.border} />
+                        <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" my="1rem">
+                            <h2>Flag</h2>
+                            <img src={process.env.PUBLIC_URL + props.selectedCountry.flag} alt="Flag" height="32px" />
+                        </Box>
+                        {
+                            [['Capital', props.selectedCountry.capital], ['Population', props.selectedCountry.population], ['Region', props.selectedCountry.region]].map((text) => {
+                                return (
+                                    <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" my="1rem">
+                                        <h2>{text[0]}</h2>
+                                        <p>{text[1]} </p>
+                                    </Box>
+                                )
+                            })
+                        }
+                    </Box>
+                </ListItem>
+            </List>
+        </div>
+    );
+
+
+    return (
+        <>
+            <div>
+                {['right'].map((anchor) => (
+                    <React.Fragment key={anchor}>
+                        <Drawer anchor={anchor} open={props.state[anchor]} onClose={props.toggleDrawer(anchor, false)}>
+                            {list(anchor)}
+                        </Drawer>
+                    </React.Fragment>
+                ))}
+            </div>
+
+
+        </>
+    )
+}
+
+export default Sidebar
